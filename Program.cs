72ab89c441f706a0de43aac66ce318dc0c6da944
Rafael <faelmota7@gmail.com>
2022-10-03 20:03:05 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿double primeiro, segundo, resultado;
+
+Console.Write("\nDigite o primeiro número: ");
+    primeiro = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+     segundo = Convert.ToDouble(Console.ReadLine());
+
+resultado = primeiro + segundo;
+
+Console.Write($"\n{primeiro} + {segundo} = ");
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+
+Console.WriteLine($"{resultado}\n");
+
+Console.ResetColor();
+
+
+
+
